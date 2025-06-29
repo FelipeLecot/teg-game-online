3e6ca2c32d281c55c70829ce469a5755a1c0c7f2
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2025-06-29 04:00:36 -0300

    [ft] end-turn endpoint

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,8 @@ import express from 'express';
 import http from 'http';
 import { parseCookies } from './helpers/getCookies.ts'
 import { Server, Socket } from 'socket.io';
-import { Game } from './types.ts'
+import { Game, Country } from './types.ts'
 import { getPlayerAndGameInfo } from './helpers/getPlayerAndGameInfo.ts';
-
-// attack-country {
-//  set game attack
-// }
 
 const gamesArr: Game[] = []
 const app = express();
@@ -24,7 +20,7 @@ io.on('connection', (socket: Socket) => {
   console.log(`Cliente conectado: ${socket.id}`);
   const cookies = parseCookies(socket.handshake.headers.cookie);
 
-  socket.on('play-effect-card', ({ cardName }) => {
+  socket.on('play-effect-card', (cardName: string) => {
     try {
       const { game, playerIndex, player } = getPlayerAndGameInfo(gamesArr, cookies);
       const hasCard = player.hasEffectCard(cardName);
@@ -38,17 +34,25 @@ io.on('connection', (socket: Socket) => {
       return true;
     } catch (error) {
       console.error(error);
-      socket.emit('error', error.message)
+      socket.emit('error', error.message);
     }
   });
 
   socket.on('end-turn', () => {
-    const { game, playerIndex, player } = getPlayerAndGameInfo(gamesArr, cookies);
-    if (!player.isPlayerTurn()) {
-      return socket.emit('error', `Is not ${player.name} turn`)
+    try {
+      const { game, playerIndex, player } = getPlayerAndGameInfo(gamesArr, cookies);
+      if (!player.isPlayerTurn()) throw new Error(`Is not ${player.name} turn`);
+
+      const turn = game.advanceTurn();
+
+      io.emit('turn-changed', turn);
+    } catch (error) {
+      console.error(error);
+      socket.emit(error.message);
     }
-    game.advanceTurn();
-    io.emit('turn-changed', playerIndex);
+  });
+
+    }
   });
 });
 
