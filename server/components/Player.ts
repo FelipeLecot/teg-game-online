4cdc3db2d9bd18ec1f3ec5ed8d06a56355e0cf4e
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2025-06-29 04:18:43 -0300

    Added player and game constructors (#10)

---
+++ server/components/Player.ts
@@ -0,0 +1,33 @@
+import { CountryType, EffectCardType, PlayerType } from "../types";
+
+const PLAYER_COLORS = ["red", "blue", "green", "yellow", "purple", "orange", "black", "white", "pink", "purple"];
+
+export class Player implements PlayerType {
+    name: string;
+    dicecards: number[][];
+    effectcards: EffectCardType[];
+    color: string;
+    countries: CountryType[];
+    socketId: string;
+    
+    constructor(name: string, sockerId: string = "") {
+        this.name = name;
+        this.color = PLAYER_COLORS[Math.floor(Math.random() * PLAYER_COLORS.length)];
+        this.dicecards = [];
+        this.effectcards = [];
+        this.countries = [];
+        this.socketId = sockerId;
+    }
+    
+    isPlayerTurn(): boolean {
+        return false;
+    }
+    
+    hasEffectCard(card: string): boolean {
+        return this.effectcards.some((c) => c.name === card);
+    }
+    
+    hasDiceCard(card: number[]): boolean {
+        return this.dicecards.some((d) => d.join("") === card.join(""));
+    }
+}
