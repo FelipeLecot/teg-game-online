7a58e30d2330209b680f3bf57026f1f2e1ed69cd
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2025-06-28 23:37:21 -0300

    [HOT FIX] ignore dotenv

---
+++ server/logic/cardManager.ts
@@ -0,0 +1,11 @@
+socket.on('draw-card', () => {
+    
+});
+
+socket.on('play-card', (data) => {
+
+});
+
+socket.on('end-turn', () => {
+
+});
---
+++ server/logic/turnManager.ts
@@ -0,0 +1,3 @@
+export function getCurrentPlayer(game: Game): Player
+export function endTurn(game: Game): void
+export function advanceTurn(game: Game): void
