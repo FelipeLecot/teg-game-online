d5a1082fafd6437f9660f488dd0a2a0b06187aa8
Nahuel Ibarra <92542799+Animator55@users.noreply.github.com>
2025-06-29 02:19:47 -0300

    select country

--- client/src/components/common/country_card/index.tsx
+++ client/src/components/common/country_card/index.tsx
@@ -1,11 +1,15 @@
+import { useGameActions, useGameValues } from '../../../pages/Game'
 import type { Country } from '../../../utils/types'
 import styles from './styles.module.css'
 
 
 export default function CountryCard ({country}: {country: Country}) {
+  const { setTargetCountry } = useGameActions()
+  const { targetCountry } = useGameValues()
     return <div
         key={Math.random()}
-        className={styles.card}
+        className={[styles.card, targetCountry === country.name ? styles.selected : ''].join(' ')}
+        onClick={()=>{setTargetCountry(country.name)}}
     >
         <h4>{country.name}</h4>
         <hr />
--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -11,23 +11,51 @@ import CardList from "../components/hud/card_list"
 let socket: Socket
 
 type Props = {
-    playerName: string;
-    gameKey: string;
+  playerName: string;
+  gameKey: string;
 }
 
-export default function Game ({playerName = 'Guest', gameKey = ''}: Props){
+// --- CONTEXTO DE ACCIONES ---
+type GameActionsContextType = {
+  setTargetCountry: (countryName: string | undefined) => void;
+}
+
+const GameActionsContext = React.createContext<GameActionsContextType | null>(null)
+
+export const useGameActions = () => {
+  const context = React.useContext(GameActionsContext)
+  if (!context) throw new Error("useGameActions must be used within GameActionsContext.Provider")
+  return context
+}
+
+// --- CONTEXTO DE VALORES ---
+type GameValuesContextType = {
+  targetCountry: string | undefined;
+  turn: number;
+}
+
+const GameValuesContext = React.createContext<GameValuesContextType | null>(null)
+
+export const useGameValues = () => {
+  const context = React.useContext(GameValuesContext)
+  if (!context) throw new Error("useGameValues must be used within GameValuesContext.Provider")
+  return context
+}
+
+// --- COMPONENTE GAME ---
+export default function Game({ playerName = 'Guest', gameKey = '' }: Props) {
   const [countries, setCountries] = React.useState<Country[]>(defaultCountries)
   const [turn, setTurn] = React.useState<number>(0)
   const [players, setPlayers] = React.useState<Player[]>(defaultPlayers)
+  const [targetCountry, setTargetCountry] = React.useState<string | undefined>(undefined)
   const [cards, setCards] = React.useState<number[][]>(defaultCards)
 
   React.useEffect(() => {
-    // Connect to the socket game
     socket = io(gameKey !== '' ? gameKey : "http://localhost:3000")
 
     socket.on("connect", () => {
       console.log("Connected to socket game:", socket.id)
-      setPlayers([...players, {
+      setPlayers(prev => [...prev, {
         id: socket.id ?? '',
         name: playerName,
         countries: []
@@ -35,21 +63,33 @@ export default function Game ({playerName = 'Guest', gameKey = ''}: Props){
       setCountries(defaultCountries)
     })
 
-    // update cards
     socket.on("update_cards", (newCards: number[][]) => {
       setCards(newCards)
     })
 
-    // return () => {
-    //   socket.disconnect()
-    // }
+    // return () => socket.disconnect()
   }, [])
 
+  console.log(targetCountry)
+
+  const gameActions = React.useMemo(() => ({
+    setTargetCountry,
+  }), [])
+
+  const gameValues = React.useMemo(() => ({
+    targetCountry,
+    turn,
+  }), [targetCountry, turn])
+
   return (
-    <main>
-      <CountriesList list={countries}/>
-      <PlayersList list={players} turn={turn}/>
-      <CardList list={cards} />
-    </main>
+    <GameActionsContext.Provider value={gameActions}>
+      <GameValuesContext.Provider value={gameValues}>
+        <main>
+          <CountriesList list={countries} />
+          <PlayersList list={players} />
+          <CardList list={cards} />
+        </main>
+      </GameValuesContext.Provider>
+    </GameActionsContext.Provider>
   )
 }
